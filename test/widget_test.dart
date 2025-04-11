27976fd93cf7943c7a4939801f47788c15dc8dfc
ZiaUrRehmanDev <arainzia32@gmail.com>
2025-04-11 15:26:03 +0500

    test define in github action

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,39 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:mockito/mockito.dart';
+import 'package:auth_app/features/splash/view/pages/splash_page.dart';
+import 'package:auth_app/features/splash/viewmodel/splash_viewmodel.dart';
+import 'package:auth_app/core/provdier/langauges_provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import 'package:auth_app/main.dart';
+class MockSplashViewModel extends Mock implements SplashViewModel {}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('SplashPage basic UI test', (WidgetTester tester) async {
+    final mockViewModel = MockSplashViewModel();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider<SplashViewModel>.value(value: mockViewModel),
+          ChangeNotifierProvider(create: (_) => LanguagesProvider()),
+        ],
+        child: MaterialApp(
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          locale: const Locale('en'),
+          home: const SplashPage(),
+        ),
+      ),
+    );
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Let post frame callback run
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Check that icon and texts appear
+    expect(find.byIcon(Icons.lock), findsOneWidget);
+    expect(find.text('Welcome'), findsOneWidget); // Adjust if your localized text differs
+    expect(find.text('Loading...'), findsOneWidget);
   });
 }
